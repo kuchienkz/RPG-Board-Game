df97c93012f820fc303d5ca5e0412e1fd0108287
Wahyu <wahyu.darkflame@gmail.com>
2018-11-15 10:55:54 +0700

    improves

---
+++ Assets/Scripts/CardClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public abstract class CardClass
+    {
+        public string Name;
+        public string Description;
+
+        
+    }
+}
--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -7,55 +7,31 @@ public class PlayerClass : MonoBehaviour
 {
     public string Name;
 
-    public int CurrentHealthPoint;
-    public int MaximumHealthPoint;
-    public int CurrentActionPoint;
-    public int MaximumActionPoint;
-
-    public int EXPToNextLevel;
-    public int Level;
-
     public int Gold;
-    public List<ItemClass> Inventory;
-
-    // Base Stats
-    private int base_ATK = 50;
-    private int base_DEF = 40;
-    private int base_EVA = 20;
+    public List<CardClass> Inventory;
+    
 
     // True Stats
     public int ATK;
-    public int DEF;
-    public int EVA;
-
-
-    // Job Class
-    private JobClass cls;
-    public JobClass Class
-    {
-        get { return cls; }
-        set
-        {
-            cls = value;
-            RecalculateStats();
-        }
-    }
-
+    public int LIVES;
 
     private void RecalculateStats()
     {
-        ATK = base_ATK + Class.bonus_ATK;
-        DEF = base_DEF + Class.bonus_DEF;
-        EVA = base_EVA + Class.bonus_EVA;
+        int bonus_ATK = 0;
+
+        foreach (var card in Inventory)
+        {
+            bonus_ATK += card.GetBonus_ATK();
+        }
+
+        ATK = base_ATK + bonus_ATK;  
     }
 
     // Use this for initialization
     void Start ()
     {
-        Gold = 1000;
-        Inventory = new List<ItemClass>();
-        Level = 1;
-        Class = new Adventurer();
+        Gold = 500;
+        Inventory = new List<CardClass>();
 	}
 	
 	// Update is called once per frame
